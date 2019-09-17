98286f28ec8d021af049abfe3b46f29d3b333c03
Miska0 <kovacs.kristof.jakab.95@gmail.com>
2019-09-17 17:21:28 +0200

    első óra

---
+++ src/main/java/hu/elte/issuetracker/repository/IssueRepository.java
@@ -0,0 +1,12 @@
+package hu.elte.issuetracker.repository;
+
+import hu.elte.issuetracker.model.Issue;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IssueRepository extends CrudRepository<Issue, Integer> {
+
+    Iterable<Issue> findByTitle(String title);
+
+}
