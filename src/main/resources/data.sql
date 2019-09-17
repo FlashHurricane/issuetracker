aa3db72672bbc388773833fe56709bab0ed6399d
Miska0 <kovacs.kristof.jakab.95@gmail.com>
2019-09-17 17:30:00 +0200

    első óra sqllel kiegészítve

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO issue(title, description, place, created_at, modified_at, status) VALUES('Rossz projektor', 'Nem kapcsol be a projektor', '2-209',CURRENT_TIMESTAMP(),CURRENT_TIMESTAMP(), 'NEW')
+INSERT INTO issue(title, description, place, created_at, modified_at, status) VALUES('Rossz projektor', 'Nem kapcsol be a projektor', '2-209',CURRENT_TIMESTAMP(),CURRENT_TIMESTAMP(), 'DOING')
+INSERT INTO issue(title, description, place, created_at, modified_at, status) VALUES('ZH Mód', 'Van internet', '2-209',CURRENT_TIMESTAMP(),CURRENT_TIMESTAMP(), 'DONE')
